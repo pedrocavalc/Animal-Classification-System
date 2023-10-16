1f5738a21dad5fbcc6c0ff073903097263dbd638
pedrocavalc <pedro.cavalvante@lapisco.ifce.edu.br>
2023-10-16 18:30:05 -0300

    # added change on css

--- src/streamlit/main.py
+++ src/streamlit/main.py
@@ -8,22 +8,23 @@ import time
 import threading
 import base64
 
+# Constants
 API_URL = "http://localhost:8000/predict"
+REFERENCE_RESOLUTION = 1920 * 1080
+REFERENCE_SPEED = 0.01  
+SQUARE_SIZE = 20
 stop_scanning = threading.Event()
 
 def simulate_scanning_effect(image: "PIL.Image.Image", placeholder):
+    """
+    Simulates scanning effect on the uploaded image.
+    """
     width, height = image.size
-    square_size = 20
-
     base = image.convert("RGBA")
     
-    # Parâmetros de velocidade
-    reference_resolution = 1920 * 1080
-    current_resolution = width * height
-    reference_speed = 0.01  
-    sleep_time = (current_resolution / reference_resolution) * reference_speed
-    sleep_time = max(0.001, min(sleep_time, 0.1))  
-
+    sleep_time = (width * height / REFERENCE_RESOLUTION) * REFERENCE_SPEED
+    sleep_time = max(0.001, min(sleep_time, 0.1))
+    
     is_horizontal_scan = True
     position = 0
 
@@ -32,19 +33,18 @@ def simulate_scanning_effect(image: "PIL.Image.Image", placeholder):
         draw = ImageDraw.Draw(overlay)
 
         if is_horizontal_scan:
-            for y_position in range(0, height, square_size):
-                draw.rectangle([position, y_position, position + square_size, y_position + square_size], fill=(70, 130, 255, 100))
+            for y_position in range(0, height, SQUARE_SIZE):
+                draw.rectangle([position, y_position, position + SQUARE_SIZE, y_position + SQUARE_SIZE], fill=(70, 130, 255, 100))
 
-            position += square_size
+            position += SQUARE_SIZE
             if position >= width:
                 position = 0
                 is_horizontal_scan = not is_horizontal_scan
-
         else:
-            for x_position in range(0, width, square_size):
-                draw.rectangle([x_position, position, x_position + square_size, position + square_size], fill=(70, 130, 255, 100))
+            for x_position in range(0, width, SQUARE_SIZE):
+                draw.rectangle([x_position, position, x_position + SQUARE_SIZE, position + SQUARE_SIZE], fill=(70, 130, 255, 100))
 
-            position += square_size
+            position += SQUARE_SIZE
             if position >= height:
                 position = 0
                 is_horizontal_scan = not is_horizontal_scan
@@ -54,58 +54,80 @@ def simulate_scanning_effect(image: "PIL.Image.Image", placeholder):
         
         time.sleep(sleep_time)
 
-def upload_function():    
-    uploaded_file = st.file_uploader("Choose a file", type=['jpg'])
+def send_image_to_api(image) -> requests.Response:
+    """
+    Sends image to the API endpoint for prediction.
+    """
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    image_str = buffered.getvalue()
+    files = {"file": image_str}
+    return requests.post(API_URL, files=files)
+
+def upload_function():
+    """
+    Manages the file uploading process and display predictions.
+    """
+    uploaded_file = st.file_uploader("Choose a file", type=['jpg','png'])
+    
     if uploaded_file is None:
-        st.write("Please upload a file.")
+        st.warning("Please upload a file.")
     else:
         image = Image.open(uploaded_file).convert("RGB")
         placeholder = st.empty()
         
         stop_scanning.clear()
-        thread = threading.Thread(target=simulate_scanning_effect, args=(image, placeholder))
-        add_script_run_ctx(thread)
-        thread.start()
+        scanning_thread = threading.Thread(target=simulate_scanning_effect, args=(image, placeholder))
+        add_script_run_ctx(scanning_thread)
+        scanning_thread.start()
         
         response = send_image_to_api(image)
 
         stop_scanning.set()
-        thread.join()
+        scanning_thread.join()
         placeholder.empty()
 
         if response.status_code == 200:
-            response_data = response.json()  # Parse JSON response
+            response_data = response.json()
             decoded_image = base64.b64decode(response_data["shap_image"].split(",")[1])
             prediction_image = Image.open(BytesIO(decoded_image))
-            annotated_text(f"Predicted class: ", annotation(response_data['predicted_class'], font_family="Comic Sans MS", border="1px dashed red"))
-            st.image(prediction_image, caption=f"Prediction: {response_data['predicted_class']}", use_column_width=True)
             
+            st.divider()
+
+                
+            st.markdown("## Predicted class: ", unsafe_allow_html=True)
+            annotated_text(
+                annotation(
+                    response_data['predicted_class'],
+                    font_family="Arial",
+                    border="1px solid gray",
+                    font_size="24px",
+                    font_weight="bold",
+                    background="lightyellow",
+                    color="black"
+                )
+            )
+            st.image(prediction_image, use_column_width=True)
+
+            st.markdown("</div>", unsafe_allow_html=True)  # Fim do contorno
+
+            st.markdown("## To make another prediction, upload another file above!")
+
         else:
             st.error('Error during API call')
 
-def send_image_to_api(image):
-    buffered = BytesIO()
-    image.save(buffered, format="JPEG")
-    image_str = buffered.getvalue()
-    files = {"file": image_str}
-    response = requests.post(API_URL, files=files)
-    return response
 
 def main():
     st.title("Welcome to Explainable AI app for animal classification!")
+    st.divider()
     st.sidebar.title("Informations about the project:")
-    st.sidebar.info("This project was developed by Pedro Cavalcante. [GitHub](https://github.com/pedrocavalc)")
-    st.sidebar.info("The project uses a ResNet50 model trained on the Animals-90 dataset.The objective of the project is to classify images of animals into 90 differentes species. The project uses SHAP to explain the model's predictions.")
-    st.sidebar.info("The main objective of the project is to learn MLOps techniques to serve the model in production time. Every 3 days a new model is automatically trained, the mlflow library is used to serve the models and automated tests with Jenkins to maintain the quality of the code and model before serving it to production.     If the trained model meets the necessary requirements, it is automatically sent to production and the old one archived.")
-    st.sidebar.info("For more information about the tools used and the project flowchart, see the repository.")
-    if 'button_clicked' not in st.session_state:
-        st.session_state.button_clicked = False
+    st.sidebar.info("Developed by Pedro Cavalcante. [GitHub](https://github.com/pedrocavalc)")
+    st.sidebar.info("Trained on the Animals-90 dataset using ResNet50. The project uses SHAP for predictions.")
+    st.sidebar.info("Goal: Learn MLOps techniques for production. Models retrain every 3 days, with automated tests via Jenkins and deploy with MLFlow.")
+    st.sidebar.info("For more details, check the [repository](https://github.com/pedrocavalc/Animal-Classification-System).")
+    
+    upload_function()
 
-    if st.button("Show Upload Widget", key="upload"):
-        st.session_state.button_clicked = True
-
-    if st.session_state.button_clicked:
-        upload_function()
 
 if __name__ == "__main__":
     main()
