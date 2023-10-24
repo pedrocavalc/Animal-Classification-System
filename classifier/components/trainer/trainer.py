776d61c1e599686d04d9f0cfb869f245ecd9a1c3
pedrocavalc <pedro.cavalvante@lapisco.ifce.edu.br>
2023-10-24 11:40:39 -0300

    #1 log and registry in mlflow

--- classifier/components/trainer/trainer.py
+++ classifier/components/trainer/trainer.py
@@ -1,15 +1,17 @@
 from components.model.resnet import CustomResNet50
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 import mlflow
+mlflow.set_tracking_uri('http://0.0.0.0:5000')
 
 class TrainOrchestrator:
     def __init__(self) -> None:
-        pass
+        self.client = mlflow.MlflowClient()
 
     def run(self, train_data, test_data, valid_data):
         num_classes = len(set(train_data.classes))
         model = CustomResNet50(num_classes=num_classes).get_model()
         model.summary()
+        mlflow.set_experiment("ResNet50")
         mlflow.autolog()
         with mlflow.start_run():
             history = model.fit(
@@ -25,4 +27,10 @@ class TrainOrchestrator:
                 ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=2, mode='min') 
             ]
         )
+            self.test_register_model(model, test_data)
     
+    def test_register_model(self, model,test_data):
+        acc = model.evaluate(test_data, steps=len(test_data))[1]
+        model_in_production = self.client.get_latest_versions("test", stages=["Production"])
+        print(model_in_production)
+       
